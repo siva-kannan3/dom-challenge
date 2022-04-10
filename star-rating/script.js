ab25c531db972d9f3ec8bdfa3f9302b18e3d1046
sivak <sivakwalker3@gmail.com>
2022-04-10 11:50:59 +0530

    refactor: 🎨 code optimizing

---
+++ star-rating/script.js
@@ -0,0 +1,48 @@
+function Star(el, count, callback) {
+  let active = -1;
+  const element = document.querySelector(el);
+  const fragment = document.createDocumentFragment();
+  for (let i = 1; i <= count; i++) {
+    const iElem = document.createElement("i");
+    iElem.classList.add("fa", "fa-star", "fa-3x");
+    iElem.dataset.ratingVal = i;
+    fragment.appendChild(iElem);
+  }
+  element.appendChild(fragment);
+  element.addEventListener("mouseover", onMouseOver);
+  element.addEventListener("click", onClick);
+  element.addEventListener("mouseleave", onMouseLeave);
+
+  function onMouseOver(e) {
+    const ratingVal = e.target.dataset.ratingVal;
+    if (!ratingVal) {
+      return;
+    }
+    fill(ratingVal);
+  }
+
+  function fill(ratingVal) {
+    for (let i = 0; i < count; i++) {
+      if (i < ratingVal) {
+        element.children[i].classList.add("fill");
+      } else {
+        element.children[i].classList.remove("fill");
+      }
+    }
+  }
+
+  function onMouseLeave(e) {
+    fill(active);
+  }
+
+  function onClick(e) {
+    active = e.target.dataset.ratingVal;
+    fill(active);
+    callback(active);
+  }
+}
+
+function getStar(value) {
+  document.getElementById("display-star").innerHTML = value;
+}
+Star("#star", 15, getStar);
